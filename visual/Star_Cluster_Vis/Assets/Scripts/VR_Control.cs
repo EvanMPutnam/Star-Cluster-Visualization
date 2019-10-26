79d89b138dc89a3ae2c04eed12c153602ad11e56
Evan Putnam <emput1998@gmail.com>
2019-10-25 23:39:02 -0400

    Revising controls to include rotation.

--- visual/Star_Cluster_Vis/Assets/Scripts/VR_Control.cs
+++ visual/Star_Cluster_Vis/Assets/Scripts/VR_Control.cs
@@ -11,7 +11,8 @@ public class VR_Control : MonoBehaviour
     //Private transform (Assigned once for performance)
     private Transform particleContainerTransform;
     
-    
+
+
     //Constant for scaling object.
     public float SCALE_INC = 0.0005f;
     //Constant for base scale factor
@@ -20,6 +21,8 @@ public class VR_Control : MonoBehaviour
     public float MOVE_INC = 0.0025f;
     //VR controller threshold (How far controller button needs to be pressed)
     public float VR_CONTROLLER_THRESHOLD = 0.5f;
+    //Value for rotation speed.
+    public float ROTATION_SPEED = 0.1f;
 
 
 
@@ -37,7 +40,6 @@ public class VR_Control : MonoBehaviour
         //Vector for inputs on right thumbstick.
         Vector2 vec = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
 
-
         //Movement z direction
         if(vec.y > VR_CONTROLLER_THRESHOLD)
         {
@@ -47,6 +49,7 @@ public class VR_Control : MonoBehaviour
         {
             particleContainerTransform.Translate(new Vector3(0, 0, -MOVE_INC), Space.World);
         }
+        
         //Movement x direction
         if(vec.x > VR_CONTROLLER_THRESHOLD)
         {
@@ -58,27 +61,53 @@ public class VR_Control : MonoBehaviour
         }
         
         //Movement y direction
-        float r_Trigger = OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger);
-        float l_Trigger = OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger);
-        if(r_Trigger > VR_CONTROLLER_THRESHOLD)
+        float rUnderTrigger = OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger);
+        float lUnderTrigger = OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger);
+        if(rUnderTrigger > VR_CONTROLLER_THRESHOLD)
         {
             particleContainerTransform.Translate(new Vector3(0, MOVE_INC, 0), Space.World);
+            
         }
-        else if(l_Trigger > VR_CONTROLLER_THRESHOLD)
+        else if(lUnderTrigger > VR_CONTROLLER_THRESHOLD)
         {
             particleContainerTransform.Translate(new Vector3(0, -MOVE_INC, 0), Space.World);
         }
 
-        //Scaling up and down.
-        vec = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
-        if(vec.y > VR_CONTROLLER_THRESHOLD)
+        //Scaling of starfield.
+        float rTopTrigger = OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);
+        float lTopTrigger = OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger);
+        if(rTopTrigger > VR_CONTROLLER_THRESHOLD)
         {
             particleContainerTransform.localScale += new Vector3(SCALE_INC, SCALE_INC, SCALE_INC);
         }
-        else if(vec.y < -VR_CONTROLLER_THRESHOLD)
+        else if(lTopTrigger > VR_CONTROLLER_THRESHOLD)
         {
             particleContainerTransform.localScale += new Vector3(-SCALE_INC, -SCALE_INC, -SCALE_INC);
         }
+
+
+        //Rotation of starfield.
+        vec = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+        //Rotate around the y axis
+        if(vec.y > VR_CONTROLLER_THRESHOLD)
+        {
+           particleContainerTransform.Rotate(ROTATION_SPEED, 0, 0, Space.Self);
+        }
+        else if(vec.y < -VR_CONTROLLER_THRESHOLD)
+        {
+            particleContainerTransform.Rotate(-ROTATION_SPEED, 0, 0, Space.Self);   
+        }
+
+        //Rotate around the x axis.
+        if(vec.x > VR_CONTROLLER_THRESHOLD)
+        {
+            particleContainerTransform.Rotate(0, ROTATION_SPEED, 0, Space.Self);
+        }
+        else if(vec.x < -VR_CONTROLLER_THRESHOLD)
+        {
+            particleContainerTransform.Rotate(0,  -ROTATION_SPEED, 0, Space.Self);
+        }
+
 
     }
 }
