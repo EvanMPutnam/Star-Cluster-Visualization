19157564cbad0974dfbd2e4046b8de0a84469e2f
lauramv1832 <lauravannozzi@yahoo.com>
2019-12-02 15:45:09 -0500

    Added radius function. pleiades_final_output.csv contains a radius column

--- data_sets/data_script.py
+++ data_sets/data_script.py
@@ -20,6 +20,23 @@ def y(R, b, l):
 def z(R, b):
     return R * math.sin(b)
 
+def luminosity(m, d):
+    return 4*math.pi*math.pow(d, 2)*m
+
+def solar_lum(l):
+    return l/(3.828*math.pow(10, 26))
+
+def temp(l):
+    sun = 5778
+    frac = 1/4
+    return math.pow(l, frac) * sun
+
+#σ = 5.670374419...×10−8 W⋅m−2⋅K−4
+def radius(l, t):
+    const = 5.67 * math.pow(10, -8)
+    ans = l/(4*math.pi*const*math.pow(t, 4))
+    return math.log10(math.sqrt(ans))
+
 
 def parse(csv_file):
     df = pd.read_csv(csv_file)
@@ -28,6 +45,11 @@ def parse(csv_file):
     df.loc[:, 'x'] = df.apply(lambda r: x(r['R'], r['b'], r['l']), axis=1)
     df.loc[:, 'y'] = df.apply(lambda r: y(r['R'], r['b'], r['l']), axis=1)
     df.loc[:, 'z'] = df.apply(lambda r: z(r['R'], r['b']), axis=1)
+    df.loc[:, 'luminosity'] = df.apply(lambda r: luminosity(r['phot_g_mean_mag'], r['R']), axis=1)
+    df.loc[:, 'solar_luminosity'] = df.apply(lambda r: solar_lum(r['luminosity']), axis=1)
+    df.loc[:, 'temperature'] = df.apply(lambda r: temp(r['solar_luminosity']), axis=1)
+    df.loc[:, 'radius'] = df.apply(lambda r: radius(r['phot_g_mean_mag'], r['temperature']), axis=1)
+
     df.to_csv(csv_file.split('.')[0] + "_output.csv")
 
 
