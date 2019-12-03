a4c922be8c9218e5ee690ed59df05bc8bed32cc0
Evan Putnam <emput1998@gmail.com>
2019-12-03 03:53:47 -0500

    Updating scripts for accurate representation.  Need to confirm with Laura

--- visual/Star_Cluster_Vis/Assets/Scripts/Generic_Parser.cs
+++ visual/Star_Cluster_Vis/Assets/Scripts/Generic_Parser.cs
@@ -2,6 +2,14 @@
 
 public class Generic_Parser {
 
+
+    private const string X_COL = "x";
+    private const string Y_COL = "y";
+    private const string Z_COL = "z";
+    private const string R_COL = "phot_rp_mean_mag";
+    private const string G_COL = "phot_g_mean_mag";
+    private const string B_COL = "phot_bp_mean_mag";
+
     public static StarContainer Create_Star_Cluster(TextAsset file, int mod_val, float scale_val){
         StarContainer s = new StarContainer(mod_val, scale_val);
         string[] lines = file.text.Split('\n');
@@ -20,27 +28,32 @@ public class Generic_Parser {
 
         int count = 0;
 
+        float max_r_val = 0;
+        float max_g_val = 0;
+        float max_b_val = 0;
+
         bool first_val = true;
         foreach (string line in lines){
             if(first_val){
                 string[] line_parts = line.Split(',');
                 for(int i = 0; i < line_parts.Length; i++){
-                    if(line_parts[i].ToLower().Equals("x")){
+                    line_parts[i] = line_parts[i].Trim();
+                    if(line_parts[i].ToLower().Equals(X_COL)){
                         x_index = i;
                     }
-                    if(line_parts[i].ToLower().Equals("y")){
+                    if(line_parts[i].ToLower().Equals(Y_COL)){
                         y_index = i;
                     }
-                    if(line_parts[i].ToLower().Equals("z")){
+                    if(line_parts[i].ToLower().Equals(Z_COL)){
                         z_index = i;
                     }
-                    if(line_parts[i].ToLower().Equals("r")){
+                    if(line_parts[i].ToLower().Equals(R_COL)){
                         r_index = i;
                     }
-                    if(line_parts[i].ToLower().Equals("g")){
+                    if(line_parts[i].ToLower().Equals(G_COL)){
                         g_index = i;
                     }
-                    if(line_parts[i].ToLower().Equals("b")){
+                    if(line_parts[i].ToLower().Equals(B_COL)){
                         b_index = i;
                     }
                 }
@@ -51,14 +64,33 @@ public class Generic_Parser {
                     ColorIndex ci = null;
                     //If color found then populate it.
                     if(rgb_found){
-                        ci = new ColorIndex(float.Parse(line_parts[r_index]),
+                        try{
+                            ci = new ColorIndex(float.Parse(line_parts[r_index]),
                                         float.Parse(line_parts[g_index]), 
                                         float.Parse(line_parts[b_index]));
+                            Debug.Log(line_parts[r_index]);
+                            if(float.Parse(line_parts[r_index]) > max_r_val){
+                                max_r_val = float.Parse(line_parts[r_index]);
+                            }
+                            if(float.Parse(line_parts[g_index]) > max_g_val){
+                                max_g_val = float.Parse(line_parts[g_index]);
+                            }
+                            if(float.Parse(line_parts[b_index]) > max_b_val){
+                                max_b_val = float.Parse(line_parts[b_index]);
+                            }
+                        }catch{
+                            ci = new ColorIndex(15.0f, 15.0f, 15.0f);
+                        }
                     }
                     //Populate star container with a new star. (y,z are flipped for unity.)
                     s.addStar(float.Parse(line_parts[x_index]), 
                                 float.Parse(line_parts[z_index]), 
                                 float.Parse(line_parts[y_index]), ci);
+                    if (count == 2){
+                        Debug.Log(line_parts[x_index]);
+                        Debug.Log(line_parts[y_index]);
+                        Debug.Log(line_parts[z_index]);
+                    }
                     
                 }
                 count += 1;
@@ -74,7 +106,9 @@ public class Generic_Parser {
             }
         }
 
-        Debug.Log(count);
+        s.max_r_val = max_r_val;
+        s.max_g_val = max_g_val;
+        s.max_b_val = max_b_val;
         return s;
     }
 
--- visual/Star_Cluster_Vis/Assets/Scripts/ParticleCSV.cs
+++ visual/Star_Cluster_Vis/Assets/Scripts/ParticleCSV.cs
@@ -47,6 +47,10 @@ public class StarContainer{
 
 	public float scaleVal { get; private set; }
 
+	public float max_r_val {get; set;}
+	public float max_g_val {get; set;}
+	public float max_b_val {get; set;}
+
 	public StarContainer(int modVal, float scaleVal){
 		this.stars = new List<Star>();
 		this.colorsRGB = new List<ColorIndex>();
@@ -92,51 +96,16 @@ public abstract class ParseStars{
 /// </summary>
 public class ParticleCSV : MonoBehaviour {
 
-
-	/// <summary>
-	/// Whether or not the stars are color accurate
-	/// </summary>
-	public Boolean COLOR_ACCURATE = true;
-
-
-	public Boolean PLEIADIS = true;
-
-
 	/// <summary>
 	/// Modulo value to limit ammount of stars on screen
 	/// </summary>
 	public int MOD_VAL = 1;
 
-	/// <summary>
-	/// An array of x locations for the stars
-	/// </summary>
-	private List<float> xLocs;
-
-	/// <summary>
-	/// An array of y locations for the stars
-	/// </summary>
-	private List<float> yLocs;
-
-	/// <summary>
-	/// An array of z locations for the stars
-	/// </summary>
-	private List<float> zLocs;
-
-	/// <summary>
-	/// The rgb colors of a given star.
-	/// </summary>
-	private List<ColorIndex> colorsRGB;
-
-
-	/// <summary>
-	/// Scalar to multiply the x, y, z locations by to spread them out
-	/// </summary>
-	private float scaler = 1.0f;
 
 	/// <summary>
 	/// Scalar for pleiades.
 	/// </summary>
-	private float pScalar = 1.0f;
+	private float pScalar = 0.80f;
 
 
 	/// <summary>
@@ -163,10 +132,15 @@ public class ParticleCSV : MonoBehaviour {
 	/// Start function reads in the csv files and then subsequently creates the stars.
 	/// </summary>
 	void Start () {
-		//readCsv ();
-		//createStars ();
 		StarContainer s = Generic_Parser.Create_Star_Cluster(file, MOD_VAL, pScalar);
 		Plot_Stars(s);
+	}
+
+
+	//Scaling function.
+	private float scale_rgb_value(float value, float max){
+		//Min + (Max - Min) * (new_val - Min2)/(Max2-Min2)
+		return 0.0f + (1.0f - 0.0f) * ((value-0.0f)/(max-0.0f));
 	}
 
 	private void Plot_Stars(StarContainer s){
@@ -183,10 +157,13 @@ public class ParticleCSV : MonoBehaviour {
 
 		//Get values
 		for(int i = 0; i < s.starLength(); i++){
+			//Get the particle system.
 			ParticleSystem.Particle par = arrParts[i];
+
 			//Get the star.
 			Star star = s.GetStar(i);
 			
+			//Set the position to be the position times the scalar.
 			par.position = new Vector3(
 				star.x * s.scaleVal, 
 				star.y * s.scaleVal, 
@@ -197,7 +174,10 @@ public class ParticleCSV : MonoBehaviour {
 			if (s.isTrueColor()) {
 				ColorIndex c = s.GetColor(i);
 				if (c != null) {
-					par.startColor = new Color ((float)c.r, (float)c.g, (float)c.b);
+					float r = scale_rgb_value((float)c.r, s.max_r_val);
+					float g = scale_rgb_value((float)c.g, s.max_g_val);
+					float b = scale_rgb_value((float)c.b, s.max_b_val);
+					par.startColor = new Color (r, g, b);
 				} else {
 					par.startColor = Color.white;
 				}
@@ -207,195 +187,6 @@ public class ParticleCSV : MonoBehaviour {
 		}
 		partSystem.SetParticles(arrParts, s.starLength());
 	}
-
-
-	/// <summary>
-	/// Reads the csv file and fills the x, y, z arrays with values
-	/// </summary>
-	private void readCsv(){
-		
-		DateTime d = DateTime.Now;
-
-
-		int x = 17;
-		int y = 18;
-		int z = 19;
-
-		if(PLEIADIS)
-		{
-			x = 99;
-			y = 100;
-			z = 101;
-			COLOR_ACCURATE = false;
-			scaler = 1;
-		}
-		xLocs = new List<float>();
-		yLocs = new List<float>();
-		zLocs = new List<float>();
-		colorsRGB = new List<ColorIndex> ();
-
-		int count = 0;
-		//Split the file and get the stars.
-		string[] star_file_lines = file.text.Split('\n');
-		//Process each star.
-		foreach (string line in star_file_lines) {
-			var values = line.Split (',');
-			if(count == 0)
-			{
-				Debug.Log(values[x]);
-				Debug.Log(values[y]);
-				Debug.Log(values[z]);
-			}
-			//First line should ALWAYS be an indexer to tell where values are.
-			if (count != 0) {
-				//Modulo value to help limit large datasets but keep even distribution.
-				if(count % MOD_VAL == 0){
-					//If you want color accurate to be true
-					if (COLOR_ACCURATE == true) {
-						if (values [16] == "") {
-							colorsRGB.Add (null);
-						} else {
-							colorsRGB.Add (bv2rgb (double.Parse (values [16])));
-						}
-					}
-
-
-
-					//Note that y and z coordinates are flipped because unity has a different coordinate system.  Y is up/down
-					xLocs.Add (float.Parse (values [x]));
-					yLocs.Add (float.Parse (values [z]));
-					zLocs.Add (float.Parse (values [y]));
-					partSystem.Emit (1);
-					//Counter for particle system to know how many stars.
-					starSize += 1;
-				}
-
-			} else {
-			}
-
-			count += 1;
-
-			
-
-		}
-		//print (DateTime.Now.Second - d.Second );
-	}
-
-
-
-
-	/// <summary>
-	/// Creates the stars 
-	/// </summary>
-	private void createStars(){
-		ParticleSystem.Particle[] arrParts;
-		arrParts = new ParticleSystem.Particle[starSize];
-		partSystem.GetParticles (arrParts);
-
-		int count = 0;
-		foreach( float x in xLocs){
-			ParticleSystem.Particle par = arrParts[count];
-			if(PLEIADIS)
-			{
-				par.position = new Vector3(
-					xLocs[count] * pScalar, 
-					yLocs[count] * pScalar, 
-					zLocs[count] * pScalar
-				);
-			}
-			else
-			{
-				par.position = new Vector3(
-					xLocs[count] * scaler, 
-					yLocs[count] * scaler, 
-					zLocs[count] * scaler
-				);
-			}
-			Debug.Log(par.startSize);
-			Debug.Log("Hello");
-
-
-
-			//If color accurate is true then set true color
-			if (COLOR_ACCURATE == true) {
-				ColorIndex c = colorsRGB [count];
-				if (c != null) {
-					par.startColor = new Color ((float)c.r, (float)c.g, (float)c.b);
-				} else {
-					par.startColor = Color.white;
-				}
-			}
-
-			arrParts [count] = par;
-			count += 1;
-		}
-		partSystem.SetParticles(arrParts, starSize);
-	}
-
-
-
-
-
-	/// <summary>
-	/// Function to get the rgb value of a given star from the BV value
-	/// Found here https://stackoverflow.com/questions/21977786/star-b-v-color-index-to-apparent-rgb-color
-	/// </summary>
-	/// <param name="bv">B-V value for star</param>
-	public static ColorIndex bv2rgb(double bv)    // RGB <0,1> <- BV <-0.4,+2.0> [-]
-	{
-
-		//Init vars
-		double t;
-		double r;
-		double g;
-		double b;
-
-		r=0.0; g=0.0; b=0.0;
-
-		//If below values correct o make sure they are within range
-		if (bv<-0.4)
-			bv=-0.4; 
-		if (bv> 2.0) 
-			bv= 2.0;
-		
-		if ((bv >= -0.40) && (bv < 0.00)) { 
-			t = (bv + 0.40) / (0.00 + 0.40);
-			r = 0.61 + (0.11 * t) + (0.1 * t * t); 
-		} else if ((bv >= 0.00) && (bv < 0.40)) {
-			t = (bv - 0.00) / (0.40 - 0.00);
-			r = 0.83 + (0.17 * t);
-		} else if ((bv >= 0.40) && (bv < 2.10)) {
-			t = (bv - 0.40) / (2.10 - 0.40);
-			r = 1.00;
-		}
-		if ((bv >= -0.40) && (bv < 0.00)) {
-			t = (bv + 0.40) / (0.00 + 0.40);
-			g = 0.70 + (0.07 * t) + (0.1 * t * t);
-		} else if ((bv >= 0.00) && (bv < 0.40)) {
-			t = (bv - 0.00) / (0.40 - 0.00);
-			g = 0.87 + (0.11 * t);
-		} else if ((bv >= 0.40) && (bv < 1.60)) {
-			t = (bv - 0.40) / (1.60 - 0.40);
-			g = 0.98 - (0.16 * t);
-		} else if ((bv >= 1.60) && (bv < 2.00)) {
-			t = (bv - 1.60) / (2.00 - 1.60);
-			g = 0.82 - (0.5 * t * t);
-		}
-		if ((bv >= -0.40) && (bv < 0.40)) {
-			t = (bv + 0.40) / (0.40 + 0.40);
-			b = 1.00;
-		} else if ((bv >= 0.40) && (bv < 1.50)) {
-			t = (bv - 0.40) / (1.50 - 0.40);
-			b = 1.00 - (0.47 * t) + (0.1 * t * t);
-		} else if ((bv >= 1.50) && (bv < 1.94)) {
-			t = (bv - 1.50) / (1.94 - 1.50);
-			b = 0.63 - (0.6 * t * t);
-		}
-
-		//print ("Red: " + r + ", Green: "+ g + ", Blue: " + b);
-		return new ColorIndex (r, g, b);
-	}
-
 
 
 	// Update is called once per frame
